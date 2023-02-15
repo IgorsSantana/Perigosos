cabb8efd0dddc57c5248b0317ab78b8741dd3031
Igor Santana <114110235+IgorsSantana@users.noreply.github.com>
2023-02-15 15:06:23 -0300

    Add files via upload

---
+++ assets/js/script-modal.js
@@ -0,0 +1,100 @@
+function abrirModalTuty(){
+    const modal = document.getElementById('janela-modal-tuty')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-tuty') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalTarcisio(){
+    const modal = document.getElementById('janela-modal-tarcisio')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-tarcisio') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalLeo(){
+    const modal = document.getElementById('janela-modal-leo')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-leo') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalIgor(){
+    const modal = document.getElementById('janela-modal-igor')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-igor') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalBrian(){
+    const modal = document.getElementById('janela-modal-brian')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-brian') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalMilao(){
+    const modal = document.getElementById('janela-modal-milao')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-milao') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalEliza(){
+    const modal = document.getElementById('janela-modal-eliza')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-eliza') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalCouto(){
+    const modal = document.getElementById('janela-modal-couto')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-couto') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalTh(){
+    const modal = document.getElementById('janela-modal-th')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-th') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
+function abrirModalNego(){
+    const modal = document.getElementById('janela-modal-nego')
+    modal.classList.add('abrir-modal')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal-nego') {
+            modal.classList.remove('abrir-modal')
+        }
+    })
+}
