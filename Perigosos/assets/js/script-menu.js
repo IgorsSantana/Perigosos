06d949ffeb83810fc0645759050fae266b1fa5ec
Igor Santana <114110235+IgorsSantana@users.noreply.github.com>
2023-03-06 17:56:50 -0300

    Delete Perigosos directory

--- Perigosos/assets/js/script-menu.js
+++
@@ -1,9 +0,0 @@
-function abrirMenu() {
-    const abrir = document.getElementById('menu')
-    abrir.classList.add('open')
-}
-
-function fecharMenu() {
-    const fechar = document.getElementById('menu')
-    fechar.classList.remove('open')
-}    
